4cfc2af45026a7cfa8adcc67fdf9e9d622a1ff53
shruti1803 <shrutisinha180403@gmail.com>
2025-07-30 17:55:35 +0530

    initial commit

--- server.js
+++ server.js
@@ -156,9 +156,6 @@ app.delete('/api/orders/:id', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
-
