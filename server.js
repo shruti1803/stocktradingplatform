bb16f4f6c6ae6d47c2e4900c80ec01eef82a8d50
shruti1803 <shrutisinha180403@gmail.com>
2025-07-30 17:00:38 +0530

    port

--- server.js
+++ server.js
@@ -156,6 +156,9 @@ app.delete('/api/orders/:id', async (req, res) => {
     }
 });
 
+const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
+
